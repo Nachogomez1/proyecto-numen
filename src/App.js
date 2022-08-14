5ed8153b735a48d3366d6a3c4dc60b2befc00fa8
villa008 <sergiovillagomez@gmail.com>
2022-08-14 18:59:45 -0300

    prueba

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Componentes/Header';
+
 
 function App() {
   return (
     <div className="App">
-      hola mundo!
+      <Header
+      titulo='Prueba'/>
     </div>
   );
 }
---
+++ src/Componentes/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+function Header(titulo){
+<nav className="nav-inicio">
+    <a href ="#!" className="logo">{titulo}</a>
+</nav>
+}
+
+export default Header;
