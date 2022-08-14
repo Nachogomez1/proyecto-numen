ac7ddca3cc75195bd5f25772c29cd3750bb21ebd
villa008 <sergiovillagomez@gmail.com>
2022-08-14 09:35:25 -0300

    subo pruebas

---
+++ src/Componentes/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function Header ({titulo}){
+    return(
+        <nav>
+            <div className="nav-inicio">
+                <a href="#!" className="logo">{titulo}</a>
+            </div>
+        </nav>
+    )
+}
+
+export default Header;
