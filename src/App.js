b9443fc7e9669a78a56cd43aa8bd8ac41785c570
AndrésAlfaro2022 <andresalfaro01@yahoo.com>
2022-09-05 11:52:35 -0300

    Se agrega header+banner

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@
 import React, { } from 'react';
 //import Card from './Componentes/Card';
 import Header from './Componentes/Header';
-import 'boxicons';
+//import 'boxicons';
 import './App.css';
 import Footer from './Componentes/Footer'
 import Carrousel from './Componentes/Carrousel'
